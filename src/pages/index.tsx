58d0d296ef1786febfd89f599ca4f74ede4ec9ef
viyiviyi <viyiviyi@outlook.com>
2024-09-07 11:40:34 +0800

    解决bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     useEffect(() => {
         if (query.url) {
             setConfig({
-                url: "https://proxy.eaias.com/" + (query.url as string),
+                url: query.url as string,
                 type: query.type ? (query.type as "art" | "vod") : query.url.includes("/art") ? "art" : "vod",
             });
         }
@@ -45,7 +45,9 @@ export default function Home() {
             if (!config.url) return;
             if (loading) return;
             setLoading(true);
-            return axios(config.url + "?ac=detail" + "pg=" + (page || "") + "&wd=" + (wd || "") + "&t=" + (t || ""))
+            return axios(
+                "https://proxy.eaias.com/" + config.url + "?ac=detail" + "pg=" + (page || "") + "&wd=" + (wd || "") + "&t=" + (t || "")
+            )
                 .then((res) => res.data)
                 .then((data) => {
                     if (page == 1) {
@@ -68,12 +70,12 @@ export default function Home() {
 
     useEffect(() => {
         if (!config.url) return;
-        axios(config.url + "?ac=list")
+        axios("https://proxy.eaias.com/" + config.url + "?ac=list")
             .then((res) => res.data)
             .then((data) => {
                 setTypes(data.class);
             });
-        axios(config.url + "?ac=detail")
+        axios("https://proxy.eaias.com/" + config.url + "?ac=detail")
             .then((res) => res.data)
             .then((data) => {
                 setConfig((v) => {

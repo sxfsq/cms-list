5232107594f9e41c435a2fbcee6617a520dd83c8
viyiviyi <viyiviyi@outlook.com>
2024-09-07 11:12:38 +0800

    cloudflare 构建失败了，尝试解决

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "*.*",
-                port: "**",
-                pathname: "*/**",
-            },
-        ],
-    },
+    images: { unoptimized: true },
 };
 
 export default nextConfig;
